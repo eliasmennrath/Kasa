688f977ad51defdada744203ebcafb6fe578f5a8
eliasmennrath <elias.mennrath@gmail.com>
2024-05-13 13:01:49 +0200

    Initialisation routeur

--- src/index.js
+++ src/index.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Error404 from './pages/Error404/Error404';
+
+
+// Rooter 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/",
+    element: <div></div>,
+    errorElement: <Error404 />
+  },
+]);
+
+
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//   </React.StrictMode>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <main>
+      <Header />
+      <RouterProvider router={router} />
+
+    </main>
+    <Footer />
   </React.StrictMode>
 );
 
