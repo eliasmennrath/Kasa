90007d05efea64363e01d16910c1852dba0e3fa8
eliasmennrath <elias.mennrath@gmail.com>
2024-05-24 15:01:41 +0200

    Correction erreur NavLink

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
-  RouterProvider,
+	createBrowserRouter,
+	RouterProvider,
+	Outlet
 } from "react-router-dom";
 
 
@@ -13,47 +14,52 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 import Home from './pages/Home/Home';
+import Logement from './pages/Logements/Logement';
 import About from './pages/About/About';
 import Error404 from './pages/Error404/Error404';
 
-
-// Rooter 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/",
-    element: <div></div>,
-    errorElement: <Error404 />
-  },
+	{
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/logement/:id",
+				element: <Logement />,
+			},
+				{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "*",
+				element: <Error404 />
+			}
+		]
+	}
 ]);
 
+function Layout() {
+	return (
+		<React.Fragment>
+			<main>
+				<Header />
+				<Outlet />
+			</main>
+			<Footer />
+		</React.Fragment>
+	);
+}
 
-
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//   </React.StrictMode>
-// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <main>
-      <Header />
-      <RouterProvider router={router} />
-
-    </main>
-    <Footer />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
